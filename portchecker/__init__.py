c043330fe11f70c86c5cda93b7e92e0e2d688496
dsgnr <email@danielhand.io>
2021-12-24 16:28:34 +0000

    Bump to version 0.1.3

--- portchecker/__init__.py
+++ portchecker/__init__.py
@@ -4,4 +4,4 @@ If a hostname is provided, the IP address will be obtained and queried.
 This module works with both public IPv4 and IPv6 addresses (if IPv6 is available on the client).
 Private IP addresses are excluded from the checks.
 """
-__version__ = '0.1.2'
+__version__ = '0.1.3'
